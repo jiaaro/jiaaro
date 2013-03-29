1ee9f9d4162991d69dc210906b97b6cd44dc240b
James Robert <jiaaro@gmail.com>
2013-03-29 18:04:23 -0300

    add analytics share tracking

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+$(document).on("click", ".sharebar a", function() {
+  window._gaq = window._gaq || [];
+  var network = $(this).data("network"),
+      article = $(this).data("title");
+      
+  _gaq.push(['_trackEvent', 'Sharing', network, article]);  
+});
